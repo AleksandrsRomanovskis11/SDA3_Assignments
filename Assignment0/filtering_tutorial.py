e55f13c5cec69129b865a514d9febe4415864c38
AleksandrsRomanovskis11 <romanovskis11@gmail.com>
2022-10-05 15:45:07 +0200

    xd

--- Assignment0/filtering_tutorial.py
+++ Assignment0/filtering_tutorial.py
@@ -4,14 +4,9 @@ import numpy as np
 
 image = cv2.imread('Assignment0/strumpe.jpg',0)
 
-kernel1 = np.array([[0, -1,  0],
-
-                   [-1,  5, -1],
-
-                    [0, -1,  0]])
+image_blurred = cv2.blur(src=image, ksize=(5,5))
 
 
-image_blurred = cv2.filter2D(src=image, ddepth=1, kernel=kernel1)
 
 cv2.imshow('Original',image)
 cv2.imshow('blurred',image_blurred)
